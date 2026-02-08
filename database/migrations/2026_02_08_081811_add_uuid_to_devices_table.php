7075ff8244a9ea1cc6cd5b720ea83619fcd66c7e
Ayanokozay <zayvseverybody@gmail.com>
2026-02-08 17:21:17 +0700

    sip

--- database/migrations/2026_02_08_081811_add_uuid_to_devices_table.php
+++ database/migrations/2026_02_08_081811_add_uuid_to_devices_table.php
@@ -4,25 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::table('devices', function (Blueprint $table) {
-            $table->uuid('uuid')->unique()->after('id');
+            if (!Schema::hasColumn('devices', 'uuid')) {
+                $table->char('uuid', 36)->after('id');
+            }
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('devices', function (Blueprint $table) {
-            $table->dropColumn('uuid');
+            if (Schema::hasColumn('devices', 'uuid')) {
+                $table->dropColumn('uuid');
+            }
         });
     }
 };
