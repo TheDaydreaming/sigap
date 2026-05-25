a8c275337d3620648a148a1e485863b1252e410b
Ayanokozay <zayvseverybody@gmail.com>
2026-05-25 11:15:18 +0700

    config: programmatically redirect cache path to /tmp on Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,26 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 try {
+    if (isset($_SERVER['VERCEL']) || getenv('VERCEL') || str_contains(__DIR__, '/var/task')) {
+        $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+        $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+        $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+        $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+        $_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+        
+        $_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+        $_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+        $_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+        $_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+        $_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+
+        putenv('APP_SERVICES_CACHE=/tmp/services.php');
+        putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+        putenv('APP_CONFIG_CACHE=/tmp/config.php');
+        putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+        putenv('APP_EVENTS_CACHE=/tmp/events.php');
+    }
+
     return Application::configure(basePath: dirname(__DIR__))
         ->withRouting(
             web: __DIR__.'/../routes/web.php',
