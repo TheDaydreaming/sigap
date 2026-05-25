7a0e50240ec23391821207907fd0eac33b8c0467
Ayanokozay <zayvseverybody@gmail.com>
2026-05-25 10:53:45 +0700

    debug: lacak error booting awal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,21 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+try {
+    return Application::configure(basePath: dirname(__DIR__))
+        ->withRouting(
+            web: __DIR__.'/../routes/web.php',
+            commands: __DIR__.'/../routes/console.php',
+            health: '/up',
+        )
+        ->withMiddleware(function (Middleware $middleware): void {
+            $middleware->trustProxies(at: '*');
+        })
+        ->withExceptions(function (Exceptions $exceptions): void {
+            //
+        })->create();
+} catch (\Throwable $e) {
+    error_log("SIGAP_BOOT_ERROR: " . $e->getMessage() . " in " . $e->getFile() . " on line " . $e->getLine());
+    error_log($e->getTraceAsString());
+    throw $e;
+}
